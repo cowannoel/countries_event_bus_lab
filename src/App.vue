123d76ee495b1e99bf0b92b560776d94933b83b7
Jayno17 <julia.nowak@gmail.com>
2020-02-26 16:27:56 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Countries List</h1>
+     <div>
+       <countries-list :countries='countries'></countries-list>
+       <country-detail :country='selectedCountry'></country-detail>
+     </div>
+
   </div>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
+import {eventBus} from './main.js'
 
 export default {
-  name: 'App',
+  name: 'app',
+   data(){
+     return{
+       countries: [],
+       selectedCountry: null
+     }
+   },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => this.countries = data)
+
+    eventBus.$on('country-selected', (country) =>{
+      this.selectedCountry = country;
+    })
+  },
+
   components: {
-    HelloWorld
+   "countries-list": CountriesList,
+   "country-detail": CountryDetail
+
   }
 }
 </script>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div v-if="country">
+    <h3>{{country.name}}</h3>
+    <p>Capital: {{country.capital}} </p>
+    <p>Population: {{country.population}} </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-selected',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
